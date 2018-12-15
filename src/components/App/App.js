b63828be20b6ddc77354243c934e452b9035008f
Carson Otto <carsonhockey15@gmail.com>
2018-12-15 12:51:23 -0600

    Shows DB data on DOM

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>App Page</p>
+        <p>Project Page</p>
         <ProjectList />
       </div>
     );
